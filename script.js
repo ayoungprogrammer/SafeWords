7e09919548ed140d0d7921cc002521c7a5e92a6c
Michael <michaelyoung1995@gmail.com>
2014-01-10 13:54:32 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,174 @@
+var map = {
+		"f+u*c+k+":[],
+		"s+h+i+t+":[],
+		"\\bc+r+a+p+":[],
+		"ni+g+a+":[],
+		"ballsack":[],
+		"niglet":[],
+		"nutsack":[],
+		"ni+gg+e+r+":[],
+		"f+a+g+":[],
+		"b+i+t+c+h+":[],
+		"\\bass+\\b":[],
+		"\\basses+\\b":[],
+		"ass+ho+le":[],
+		"c+u+n+t+":[],
+		"\\bc+o+c+k+s*\\b":[],
+		"c+h+i+n+k+":[],
+		"s+l+u+t+":[],
+		"d+o+u+c+h+e+":[],
+		"d+i+c+k+":[],
+		"d+y+k+e+":[],
+		"r+e+t+a+r+d+":["retardant"],
+		"g+oo+k+":[],
+		"g+oo+c+h+":[],
+		"ga+ywa+d+":[],
+		"gaa+y":[],
+		"lesbo+":["blesbok"],
+		"\\bpri+cks*\\b":[],
+		"ti+t+s":[],
+		"pu+s+y+":[],
+		"quee+r+":[]
+		
+		};
+
+
+
+var nodes = getTextNodes(document);
+var tables = [];
+
+for(var i=0;i<nodes.length;i++){
+	var table = splitStr(nodes[i].data);
+	tables.push(table);
+}
+
+var hashes = {};
+for(word in map){
+	hashes[word] = hashCode(word);
+}
+
+//console.log("Running script");
+//var exp1 = new RegExp("t+e*s+t+","i");
+//console.log(exp1.test("txsstt"));
+
+for(var j=0;j<tables.length;j++){
+	
+	var offset = 0;
+	
+	for(var k=0;k<tables[j].length;k++){
+		
+		var word = tables[j][k].word;
+		
+		for(exp in map){	
+			
+			var regexp = new RegExp(exp,"gi");
+			
+			
+			
+			if(regexp.test(tables[j][k].word)){
+				
+				
+				var test = false;
+				for(var w=0;w<map[exp].length;w++){
+					var whiteExp = new RegExp(map[exp][w]);
+					if(whiteExp.test(tables[j][k].word)){
+						console.log('safe: '+tables[j][k].word);
+						test = true;
+					}
+				}
+				if(!test){
+					var replace = "@!$#@$@!@#$@#$&#".substring(0,Math.min(word.length,15));//map[word];
+					var data = nodes[j].data;
+					
+					console.log(tables[j][k].word);
+					
+					
+					var pre = data.substring(0,tables[j][k].pos+offset);
+					var suf = data.substring(tables[j][k].pos+offset+word.length,data.length);
+					nodes[j].data = pre+replace+suf;
+					offset += replace.length-word.length;
+				}
+			}
+		}
+	}
+}
+
+function hashCode(str){
+	str = str.toLowerCase();
+    var hash = 0, i, char;
+    if (str.length == 0) return hash;
+    for (i = 0, l = this.length; i < l; i++) {
+        char  = str.charCodeAt(i);
+        hash  = ((hash<<5)-hash)+char;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+}
+
+
+
+function getTextNodes(root){
+	var nodeStack = [];
+	getNodes(root,nodeStack);
+	return nodeStack;
+}
+
+function getNodes(node,nodeStack){
+	if(node.nodeType==3){
+		nodeStack.push(node);
+	}else {
+		var child = node.firstChild, nextChild;
+		while(child){
+			nextChild = child.nextSibling;
+			getNodes(child,nodeStack);
+			child = nextChild;
+		}
+	}
+}
+
+function isUpperCase(c){
+	return (c>='A'&&c<='Z');
+}
+
+function isLetter(c){
+	return (c>='A'&&c<='Z')||(c>='a'&&c<='z');
+}
+
+function splitStr(str){
+	var tokens = [];
+	var begin = 0;
+	var end = 0;
+	var word = false;
+	
+	while(end < str.length){
+		if(word){
+			if(!isLetter(str.charAt(end))){
+				if(end-begin>0){
+					var word = str.substring(begin,end);
+					var hash = hashCode(word);
+					var obj = {'pos':begin,
+							'word':word,
+							'hash': hash};
+					tokens.push(obj);
+				}
+				word = false;
+			}
+		}else {
+			if(isLetter(str.charAt(end))){
+				begin = end;
+				word = true;
+			}
+		}
+		end++;
+	}
+	if(end-begin>0&&word){
+		var word = str.substring(begin,end);
+		var hash = hashCode(word);
+		var obj = {'pos':begin,
+				'word':word,
+				'hash': hash};
+		tokens.push(obj);
+	}
+	return tokens;
+}
+
